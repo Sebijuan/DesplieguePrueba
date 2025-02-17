748518b99e837be5e94e2ad675b692bf08bc8d88
Sebijuan <sjuacoc2t@adaits.es>
2025-02-17 18:21:04 +0100

    actualizacion

--- Backend-proyect/src/controllers/userController.js
+++ Backend-proyect/src/controllers/userController.js
@@ -1,14 +1,31 @@
-const User = require('../models/userModel');
+let users = []; // Array en memoria para almacenar usuarios
 
-const getUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+exports.getAllUsers = (req, res) => {
+    res.json(users);
 };
 
-module.exports = {
-  getUser,
+exports.createUser = (req, res) => {
+    const { name, email } = req.body;
+    const newUser = { id: users.length + 1, name, email };
+    users.push(newUser);
+    res.status(201).json(newUser);
+};
+
+exports.updateUser = (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+    const user = users.find(u => u.id === parseInt(id));
+
+    if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
+
+    user.name = name || user.name;
+    user.email = email || user.email;
+    
+    res.json(user);
+};
+
+exports.deleteUser = (req, res) => {
+    const { id } = req.params;
+    users = users.filter(u => u.id !== parseInt(id));
+    res.json({ message: "Usuario eliminado" });
 };
