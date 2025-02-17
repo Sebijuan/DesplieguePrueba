02f14d4ae41ed489e0b10adcba94db188c763b47
Sebijuan <sjuacoc2t@adaits.es>
2025-02-17 18:04:26 +0100

    actualizacion

---
+++ Backend-proyect/src/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_HOST, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('Error connecting to MongoDB', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
