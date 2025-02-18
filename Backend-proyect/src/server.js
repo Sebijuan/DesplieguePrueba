c8f04c1dea8a1a540117d6785da79394ec573489
Sebijuan <sjuacoc2t@adaits.es>
2025-02-18 01:49:20 +0100

    actualizacion

--- Backend-proyect/src/server.js
+++ Backend-proyect/src/server.js
@@ -1,20 +1,31 @@
+const express = require('express');
 const mongoose = require('mongoose');
-const app = require('./app');
+const cors = require('cors');
+const userRouter = require('./routes/userRoutes');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
+
+// Connect to MongoDB
+const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI, {
     serverSelectionTimeoutMS: 20000, // Increase timeout to 20 seconds
     socketTimeoutMS: 45000, // Increase socket timeout to 45 seconds
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => {
+    console.error('MongoDB connection error:', err);
     process.exit(1); // Exit the process if the connection fails
 });
 
+// Routes
+app.use('/api/users', userRouter);
+
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 }).on('error', (err) => {
