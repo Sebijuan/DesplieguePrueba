ceb9d39775bbf48fb580813d39bc08b3523435da
Sebijuan <sjuacoc2t@adaits.es>
2025-03-04 15:54:38 +0100

    actualizacion

--- Backend-proyect/src/routes/userRoutes.js
+++ Backend-proyect/src/routes/userRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.get('/users', userController.getAllUsers);
-router.post('/users', userController.createUser);
-router.put('/users/:id', userController.updateUser);
-router.delete('/users/:id', userController.deleteUser);
+const router = express.Router();
+
+// Obtener todos los usuarios
+router.get('/api/users', userController.getAllUsers);
+
+// Crear un usuario
+router.post('/api/users', userController.createUser);
+
+// Actualizar un usuario
+router.put('/api/users/:id', userController.updateUser);
+
+// Eliminar un usuario
+router.delete('/api/users/:id', userController.deleteUser);
 
 module.exports = router;
